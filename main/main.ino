4f911793bf916a2b7790a55be06ee6723be81a73
Vitor Salla de Mellos <vitor.salla@math.marketing>
2023-11-08 18:31:50 -0300

    added reading of switch

--- main/main.ino
+++ main/main.ino
@@ -2,6 +2,7 @@
 #include <SoftwareSerial.h>
 
 const uint8_t ledPins[3] = {13, 12, 11}; // leds
+const uint8_t switchPins[3] = {4, 3, 2}; // dip switch
 const byte potenciometroPin = A0; // potênciometro
 
 const uint8_t rxPin = 7;
@@ -12,6 +13,7 @@ SoftwareSerial mySerial(rxPin, txPin);
 ModbusRTUSlave modbus(mySerial, dePin); // porta serial com pino do enable rs485
 
 bool leds[3];
+bool switchs[3];
 uint16_t potenciometros[1];
 
 union PotenciometroUnion {
@@ -25,15 +27,22 @@ void setup() {
   pinMode(ledPins[0], OUTPUT);
   pinMode(ledPins[1], OUTPUT);
   pinMode(ledPins[2], OUTPUT);
+  pinMode(switchPins[0], INPUT_PULLUP);
+  pinMode(switchPins[1], INPUT_PULLUP);
+  pinMode(switchPins[2], INPUT_PULLUP);
   pinMode(potenciometroPin, INPUT);
 
   modbus.configureCoils(leds, 3);
+  modbus.configureDiscreteInputs(switchs, 3);
   modbus.configureInputRegisters(potenciometros, 1); // potenciometro int
   modbus.configureHoldingRegisters(potenciometroUnion.uint16Value, 2); // potenciometro float
   modbus.begin(1, 9600);
 }
 
 void loop() {
+  switchs[0] = !digitalRead(switchPins[0]);
+  switchs[1] = !digitalRead(switchPins[1]);
+  switchs[2] = !digitalRead(switchPins[2]);
   potenciometros[0] = analogRead(potenciometroPin);
   potenciometroUnion.floatValue = (5.0/1024.0)*potenciometros[0];
 
